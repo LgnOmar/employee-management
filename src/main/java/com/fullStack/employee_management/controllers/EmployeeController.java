bf6cc5e89555c0d6503cc39058e6e6737f4a6f28
LAGGOUNE Omar <dogie4bogie@gmail.com>
2026-05-08 03:17:12 +0100

    implement GET/POST/DELETE tested with Bruno

--- src/main/java/com/fullStack/employee_management/controllers/EmployeeController.java
+++ src/main/java/com/fullStack/employee_management/controllers/EmployeeController.java
@@ -2,14 +2,9 @@ package com.fullStack.employee_management.controllers;
 
 
 import com.fullStack.employee_management.entities.Employee;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -17,46 +12,42 @@ import java.util.UUID;
 @RequestMapping("/employees")
 public class EmployeeController {
 
-    ArrayList<Employee> employees = new ArrayList<>(
-            List.of(
-                    new Employee(
-                            UUID.randomUUID(),
-                            "Ahmed",
-                            "LAGGOUNE",
-                            "laggouneahmedomar@gmail.com",
-                            "0657599501",
-                            LocalDate.of(2024, 2, 24),
-                            "Software Engineer",
-                            UUID.randomUUID()
-                    ),
-                    new Employee(
-                            UUID.randomUUID(),
-                            "Omar",
-                            "LAARBI",
-                            "omarlaarbi@gmail.com",
-                            "0542834640",
-                            LocalDate.of(2024, 2, 24),
-                            "DevOps Engineer",
-                            UUID.randomUUID()
-                    )
-
-            )
-    );
+    ArrayList<Employee> employees = new ArrayList<>();
 
 
     @GetMapping
-    public ArrayList<Employee> findAll(){
+    public ArrayList<Employee> findAll() {
         return employees;
     }
 
     @GetMapping("/{employeeId}")
-    public Optional<Employee> findOne(@PathVariable UUID employeeId){
+    public Optional<Employee> findOne(@PathVariable UUID employeeId) {
 
         Optional<Employee> employee = employees.stream()
                 .filter(emp -> emp.getId().equals(employeeId))
                 .findFirst();
+        return employee;
+    }
+
+    @DeleteMapping("/{employeeId}")
+    public void deleteOne(@PathVariable UUID employeeId) {
+        Optional<Employee> employee = employees.stream()
+                .filter(emp -> emp.getId().equals(employeeId))
+                .findFirst();
+        if (employee.isPresent()) {
+            employees.remove(employee.get());
+        }
+    }
+
+
+    @PostMapping
+    public Employee createOne(@RequestBody Employee employee) {
+        employee.setId(UUID.randomUUID());
+        employee.setDepartmentId(UUID.randomUUID());
+        employees.add(employee);
 
         return employee;
+
     }
 
 }
